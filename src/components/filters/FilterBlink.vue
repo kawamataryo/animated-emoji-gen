f693bc639a75c87aa8522d6a30ad1574062de5f9
kawamataryou <ba068082@gmail.com>
2021-08-11 12:56:42 +0900

    fix: blink link

--- src/components/filters/FilterBlink.vue
+++ src/components/filters/FilterBlink.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    id="FilterWaterDrops"
+    id="FilterBlink"
     xmlns="http://www.w3.org/2000/svg"
     :viewBox="`0 0 ${viewBoxSize} ${viewBoxSize}`"
     :width="viewBoxSize"
